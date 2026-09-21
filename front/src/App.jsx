5f6fb0cdfc29f89cbf60456f4510c028e260c8c5
Marliva <cedriclevasseur76@gmail.com>
2026-09-21 13:43:17 +0200

    feat: add react-router-dom routing with admin and portfolio routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ADMIN_ROUTES } from '@/config/admin'
+
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 import Hero from './components/sections/Hero'
 import About from './components/sections/About'
 import Stack from './components/sections/Stack'
 import Projects from './components/sections/Projects'
 import Contact from './components/sections/Contact'
-import Footer from './components/layout/Footer'
 
-function App() {
+import PrivateRoute from './components/admin/PrivateRoute'
+import AdminLayout from './components/admin/AdminLayout'
+import Login from './pages/admin/Login'
+import Dashboard from './pages/admin/Dashboard'
+import Messages from './pages/admin/Messages'
+import AdminProjects from './pages/admin/Projects'
+import AdminSkills from './pages/admin/Skills'
+import AdminAbout from './pages/admin/About'
+
+function Portfolio() {
   return (
-    <div>
+    <>
       <Navbar />
       <Hero />
       <About />
@@ -16,7 +28,27 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-    </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path={ADMIN_ROUTES.login} element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route element={<AdminLayout />}>
+            <Route path={ADMIN_ROUTES.dashboard} element={<Dashboard />} />
+            <Route path={ADMIN_ROUTES.messages} element={<Messages />} />
+            <Route path={ADMIN_ROUTES.projects} element={<AdminProjects />} />
+            <Route path={ADMIN_ROUTES.skills} element={<AdminSkills />} />
+            <Route path={ADMIN_ROUTES.about} element={<AdminAbout />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
